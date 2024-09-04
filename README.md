f493c6d2586f15e6c85bc3f78bd25db727fc303e
Ivin <ivinjabraham+work@gmail.com>
2024-09-04 15:59:29 +0530

    add hello world driver

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux Driver Development
+---
+Collection of random files made while learning driver dev.
---
+++ hello_world_driver/hello.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
+
+static char* whom = "world";
+module_param(whom, charp, S_IRUGO);
+
+static int __init hello_init(void) 
+{
+    printk(KERN_ALERT "hello, world");
+    return 0;
+}
+
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("Ivin");
+MODULE_DESCRIPTION("Prints hello and goodbye");
